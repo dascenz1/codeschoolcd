982bad2d339fab19bfaaf7f007bfd7c67f4e561a
Christine Dascenzo <Christine Dascenzo>
2016-09-25 14:28:59 -0600

    added playing loop and start end phrases

--- wordgame.py
+++ wordgame.py
@@ -1,16 +1,24 @@
 def get_random_word():
     return"pizza"
 
-
 def play_word_games():
     strikes = 0
-    max_strike = 3
+    max_strikes = 3
     playing = True
 
     word = get_random_word()
 
-    
+    while playing:
+        strikes += 1
 
+        if strikes >= max_strikes:
+            playing = False
+        
+    if strikes >= max_strikes:
+        print("Sorry, game over dude.")
+    else:
+        print("winner winner!")
 
-print ("playing")
-playWordGames()
+print ("Start the Game!")
+play_word_games()
+print("End the Game")
