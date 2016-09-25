c2377bc4014b4e1799eecad352b3d50cf011b862
Christine Dascenzo <Christine Dascenzo>
2016-09-25 14:18:53 -0600

    defined get random word

--- wordgame.py
+++ wordgame.py
@@ -1,10 +1,16 @@
-def playWordGames():
-    print("Hey Hey Hey")
+def get_random_word():
+    return"pizza"
 
+
+def play_word_games():
+    strikes = 0
+    max_strike = 3
+    playing = True
+
+    word = get_random_word()
+
+    
 
 
 print ("playing")
 playWordGames()
-
-
-
